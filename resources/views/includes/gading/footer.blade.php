aae3b37a447d89bf8be8c926b68849a1adfb56cf
Rizal M <mrizalmaulana000@gmail.com>
2021-03-19 13:39:16 +0700

    update footer links

--- resources/views/includes/gading/footer.blade.php
+++ resources/views/includes/gading/footer.blade.php
@@ -2,10 +2,10 @@
 <footer id="footer4" class="page-section pt-80 pb-50 footer2-black">
     <div class="container-m-60">
       <div class="row">
-      
+
         <div class="col-md-3 col-sm-3 widget">
           <div class="logo-footer-cont">
-            <a href="index.html">
+            <a href="{{ url('/') }}">
               <img class="logo-footer" src="images/logo-footer.png" alt="logo">
             </a>
           </div>
@@ -23,31 +23,31 @@
             <a class="a-text" href="mailto:gp.mandiri@yahoo.com">gp.mandiri@yahoo.com</a>
           </div>
         </div>
-        
+
         <div class="col-md-3 col-sm-3 widget">
           <h4>NAVIGATE</h4>
           <ul class="links-list bold a-text-cont">
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="grid-system.html">ABOUT</a></li>
-            <li><a href="services.html">PROJECT</a></li>
-            <li><a href="index-portfolio.html">SERVICES</a></li>
-            <li><a href="index-blog.html">GALLERY</a></li>
-            <li><a href="index-shop.html">CONTACT</a></li>
+            <li><a href="{{ url('/') }}">HOME</a></li>
+            <li><a href="{{ url('about') }}">ABOUT</a></li>
+            <li><a href="{{ url('project') }}">PROJECT</a></li>
+            <li><a href="{{ url('service') }}">OUR SERVICES</a></li>
+            <li><a href="{{ url('gallery') }}">GALLERY</a></li>
+            <li><a href="{{ url('contact') }}">CONTACT</a></li>
           </ul>
         </div>
-        
+
         <div class="col-md-3 col-sm-3 widget">
           <h4>ABOUT US</h4>
           <ul class="links-list a-text-cont" >
-            <li><a href="about-us.html">PROFILE PERUSAHAAN</a></li>
-            <li><a href="services.html">VISI & MISI</a></li>
-            <li><a href="https://help.market.envato.com/hc/en-us">LEGALITAS</a></li>
-            <li><a href="http://themeforest.net/legal/market">CLIENT</a></li>
+            <li><a href="{{ url('about') }}">PROFILE PERUSAHAAN</a></li>
+            <li><a href="{{ url('visimisi') }}">VISI & MISI</a></li>
+            <li><a href="{{ url('legalitas') }}">LEGALITAS</a></li>
+            <li><a href="{{ url('client') }}">CLIENT</a></li>
           </ul>
         </div>
-        
-      </div>    
-      
-              
+
+      </div>
+
+
     </div>
   </footer>
